1169585dcd108d2e3a6c5b3065209ca0516bac62
Mattis Megevand <megevand.mattis@gmail.com>
2024-08-27 20:24:42 +0200

    better arch + loss beta arg + scheduler

--- train.py
+++ train.py
@@ -8,24 +8,49 @@ from torch.utils.data import Dataset, DataLoader
 import numpy as np
 from tqdm import tqdm
 import wandb
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv1 = nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm3d(out_channels)
+        self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm3d(out_channels)
+        self.relu = nn.ReLU()
+        self.downsample = nn.Conv3d(in_channels, out_channels, kernel_size=1) if in_channels != out_channels else None
+
+    def forward(self, x):
+        identity = x
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        out += identity
+        return self.relu(out)
 
 class DeepCraft(nn.Module):
     def __init__(self, num_block_types, latent_dim=128, embedding_dim=32):
         super().__init__()
-
         self.embedding = nn.Embedding(num_block_types, embedding_dim)
 
         self.encoder = nn.Sequential(
             nn.Conv3d(embedding_dim, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(64),
             nn.ReLU(),
+            ResidualBlock(64, 64),
             nn.Conv3d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(128),
             nn.ReLU(),
+            ResidualBlock(128, 128),
             nn.Conv3d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(256),
             nn.ReLU(),
+            ResidualBlock(256, 256),
             nn.Flatten()
         )
 
-        self.flatten_size = 256 * 6 * 6 * 6
+        self.flatten_size = 256 * 4 * 4 * 4
 
         self.fc_mu = nn.Linear(self.flatten_size, latent_dim)
         self.fc_var = nn.Linear(self.flatten_size, latent_dim)
@@ -33,12 +58,19 @@ class DeepCraft(nn.Module):
         self.fc_decoder = nn.Linear(latent_dim, self.flatten_size)
 
         self.decoder = nn.Sequential(
+            nn.ConvTranspose3d(256, 256, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(256),
+            nn.ReLU(),
+            ResidualBlock(256, 256),
             nn.ConvTranspose3d(256, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(128),
             nn.ReLU(),
+            ResidualBlock(128, 128),
             nn.ConvTranspose3d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(64),
             nn.ReLU(),
-            nn.ConvTranspose3d(64, num_block_types, kernel_size=5, stride=2, padding=1, output_padding=1),
-            nn.ConvTranspose3d(num_block_types, num_block_types, kernel_size=3, stride=1, padding=1)
+            ResidualBlock(64, 64),
+            nn.Conv3d(64, num_block_types, kernel_size=3, padding=1),
         )
 
     def encode(self, x):
@@ -72,10 +104,10 @@ class MinecraftDataset(Dataset):
     def __getitem__(self, idx):
         return self.data[idx]
 
-def vae_loss(recon_x, x, mu, logvar):
+def vae_loss(recon_x, x, mu, logvar, beta=1.0):
     BCE = F.cross_entropy(recon_x, x.long(), reduction='sum')
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    return BCE + KLD
+    return BCE + beta * KLD
 
 def train(model, dataloader, optimizer, device, epoch):
     model.train()
@@ -112,7 +144,7 @@ def main():
             mat_toid_mapping = json.load(f)
             num_block_types = len(mat_toid_mapping)
     else:
-        num_block_types = 437
+        num_block_types = 394
 
     wandb.init(project="deepcraft", config={
         "num_block_types": num_block_types,
@@ -138,6 +170,7 @@ def main():
 
     model = DeepCraft(num_block_types, latent_dim=latent_dim, embedding_dim=embedding_dim).to(device)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=5, verbose=True)
 
     for epoch in range(num_epochs):
         epoch_loss = train(model, dataloader, optimizer, device, epoch)
@@ -149,6 +182,8 @@ def main():
         if (epoch + 1) % 5 == 0:
             torch.save(model.state_dict(), f'deepcraft_epoch_{epoch+1}.pth')
             wandb.save(f'deepcraft_epoch_{epoch+1}.pth')
+
+        scheduler.step(epoch_loss)
 
     torch.save(model.state_dict(), 'deepcraft_final.pth')
     wandb.save('deepcraft_final.pth')
